6d4ea8229996af7e27a9eb29044756d24b545a4c
deepanshu-013 <balharadeepanshu013@gmail.com>
2026-07-06 16:02:08 +0530

    Modified the FFN, merged it into a single class.

--- notes/feedforward.md
+++ notes/feedforward.md
@@ -8,5 +8,8 @@ Implemented the Feed Forward, however there are some modifications to be made. L
 
 --> Merging the class into one class, so we avoid the variables creation. 
 
+## CHANGES
 
+Changed the Feed Forward to complete automatic. One function include everything, From linear layer to hidden layer to activation.  
 
+As per for the activation function, I am keeping it ReLU for now. 
--- transformer/feedforward.py
+++ transformer/feedforward.py
@@ -2,16 +2,19 @@ import numpy as np
 
 np.random.seed(13)  # For reproducibility
 
-class Linear:
-    def __init__(self, n_inputs, n_neurons):
-        self.weights = 0.02 * np.random.randn(n_inputs, n_neurons)
-        self.biases = np.zeros((1, n_neurons))
+class FeedForward:
+    def __init__(self, input_dim=128, hidden_dim=512):
+        self.input_dim = input_dim
+        self.hidden_dim = hidden_dim
+        self.weights1 = 0.02 * np.random.randn(input_dim, hidden_dim)
+        self.biases1 = np.zeros((1, hidden_dim))
+        self.weights2 = 0.02 * np.random.randn(hidden_dim, input_dim)
+        self.biases2 = np.zeros((1, input_dim))
 
     def forward(self, inputs):
-        self.output = np.dot(inputs, self.weights) + self.biases
-        return self.output
+        X = np.dot(inputs, self.weights1) + self.biases1
+        X = np.maximum(0, X)  # ReLU activation
+        X = np.dot(X, self.weights2) + self.biases2
+        return X
 
-class ReLU:
-    def forward(self, inputs):
-        self.output = np.maximum(0, inputs)
-        return self.output
+
