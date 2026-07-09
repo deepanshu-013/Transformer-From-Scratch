ce6842dd9d113cbebd34dff5687709bed9fec933
deepanshu-013 <balharadeepanshu013@gmail.com>
2026-07-09 14:53:24 +0530

    Added the MLM in the dataset from scratch.

--- transformer/Transformer.py
+++ transformer/Transformer.py
@@ -3,6 +3,7 @@ from embedding import Embedding
 from encoder import Encoder
 from positional_encoding import PositionalEncoding
 from lm_head import LMHead
+from mlm_dataset import MLMDataset
 
 class Transformer:
     def __init__(self, num_layers = 2):
@@ -23,6 +24,8 @@ corpus = [
 
 token = Tokenizer()
 
+mlm_dataset = MLMDataset()
+
 embedding = Embedding(5000, 128)
 
 encoder = Encoder(input_dim=128, hidden_dim=512)
@@ -35,7 +38,8 @@ lm_head = LMHead(vocab_size=5000, embedding_dim=128)
 
 token.fit(corpus)
 input_ids = token.brew('I LOVE DOGS', 10)["input_ids"]
-input_weights = embedding.forward(input_ids)
+masked_input_ids, labels = mlm_dataset.prepare(input_ids) #labels will be used for loss calculation during training
+input_weights = embedding.forward(masked_input_ids)
 positional_encoding = position.forward(input_weights.shape[0], input_weights.shape[1])
 X = positional_encoding + input_weights
 X = transformer.forward(X)
---
+++ transformer/mlm_dataset.py
@@ -0,0 +1,45 @@
+import random as rn
+from tokenizer import Tokenizer
+
+
+class MLMDataset:
+    def __init__(self):
+        self.tokenizer = Tokenizer()
+
+    def prepare(self, input_ids, mask_probability = 0.15):
+        masked_input_ids = []
+        labels = []
+        num_to_mask = max(1, round((len(input_ids) - 2) * mask_probability))
+        valid_positions = []
+
+        for token_id in input_ids:
+            masked_input_ids.append(token_id)
+            labels.append(-100)
+            if token_id not in [self.tokenizer.CLS, self.tokenizer.SEP, self.tokenizer.PAD]:
+                valid_positions.append(token_id)
+
+
+        mask_indices = rn.sample(valid_positions, num_to_mask)
+        for idx in mask_indices:
+            masked_input_ids[idx] = self.tokenizer.MASK
+            labels[idx] = input_ids[idx]
+
+        return masked_input_ids, labels
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
