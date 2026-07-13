ecbf839bd770799a486e32ff3d53f9642a36d988
deepanshu-013 <balharadeepanshu013@gmail.com>
2026-07-13 14:01:01 +0530

    Added the Backward function in the Linear Layer.

--- transformer/linear_layer.py
+++ transformer/linear_layer.py
@@ -3,8 +3,13 @@ import numpy as np
 class Linear:
     def __init__(self, embedding_dim, vocab_size):
         self.weights = np.random.randn(embedding_dim, vocab_size) * 0.02
-        self.bias = np.zeros((1, vocab_size))
+        self.biases = np.zeros((1, vocab_size))
 
     def forward(self, X):
-        return np.dot(X, self.weights) + self.bias
-    
+        self.X = X
+        return np.dot(self.X, self.weights) + self.biases
+
+    def backward(self, d_logits):
+        self.d_weights = np.dot(self.X.T, d_logits)
+        self.d_biases = np.sum(d_logits, axis=0, keepdims=True)
+        self.d_X = np.dot(d_logits, self.weights.T)
