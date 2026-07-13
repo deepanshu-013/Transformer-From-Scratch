7f2815629d1c5c89a1b26408599989bc09333125
deepanshu-013 <balharadeepanshu013@gmail.com>
2026-07-13 14:02:24 +0530

    Fixed the vocab size and added extra code for debugging and pipeline credibility check.

--- transformer/Transformer.py
+++ transformer/Transformer.py
@@ -6,6 +6,14 @@ from lm_head import LMHead
 from mlm_dataset import MLMDataset
 from loss import CrossEntropyLoss
 
+## For testing the pipeline and chekcing is model is learning or not, later will add it to separate file. ##
+class SGD:
+    def __init__(self, learning_rate=0.001):
+        self.learning = learning_rate
+    def update(self, layer):
+        layer.weights += -self.learning * layer.d_weights
+        layer.biases += -self.learning * layer.d_biases
+
 class Transformer:
     def __init__(self, num_layers = 2):
         self.encoders = [
@@ -17,17 +25,15 @@ class Transformer:
             X = encoder.forward(X)
         return X
 
-corpus = [
-    "I love dogs.",
-    "Dogs bark.",
-    "Cats meow."
-]
+# corpus = [
+#     "I love dogs.",
+#     "Dogs bark.",
+#     "Cats meow."
+# ]
 
 token = Tokenizer()
 
 mlm_dataset = MLMDataset()
-
-embedding = Embedding(5000, 128)
 
 encoder = Encoder(input_dim=128, hidden_dim=512)
 
@@ -35,20 +41,56 @@ position = PositionalEncoding()
 
 transformer = Transformer(num_layers=2)
 
-lm_head = LMHead(vocab_size=5000, embedding_dim=128)
-
 ce_loss = CrossEntropyLoss()
 
-token.fit(corpus)
-input_ids = token.brew('I LOVE DOGS', 10)["input_ids"]
-masked_input_ids, labels = mlm_dataset.prepare(input_ids) #labels will be used for loss calculation during training
+optimizer = SGD()
+
+epoches = 0
+
+token.fit('I LOVE DOGS') # Trained on single sentence to check the LM Head and the whole pipeline
+
+embedding = Embedding(len(token.word_to_id), 128) # Made the embedding layer depend on the tokenizer
+
+dic = token.brew('I LOVE DOGS', 10, decode=True)
+input_ids = dic["input_ids"]
+masked_input_ids, labels = mlm_dataset.prepare(input_ids) # labels will be used for loss calculation during training
 input_weights = embedding.forward(masked_input_ids)
 positional_encoding = position.forward(input_weights.shape[0], input_weights.shape[1])
 X = positional_encoding + input_weights
 X = transformer.forward(X)
-X = lm_head.forward(X) 
-loss = ce_loss.forward(X, labels)
 
-print(ce_loss.backward())
+lm_head = LMHead(len(token.word_to_id), embedding_dim=128) # Made the LM Head layer depend on the tokenizer
 
+logits = lm_head.forward(X) 
+loss, row = ce_loss.forward(logits, labels)
+
+##--------------------------------------------- For debugging and checking the pipeline -----------------------------------------##
+import numpy as np
+# top_5 = np.sort(row)[-5:]
+# print(top_5)
+# top5_indices = np.argpartition(row, -5)[-5:]
+# top5_list = top5_indices.tolist()
+# print(top5_indices)
+# for i in token.id_to_word:
+#     if i in top5_list:
+#         print(token.id_to_word[i])
+# print(loss)
+
+while epoches != 100:
+    dlogits = ce_loss.backward()
+    lm_head.linear.backward(dlogits)
+    optimizer.update(lm_head.linear)
+    logits = lm_head.forward(X)
+    loss, row = ce_loss.forward(logits, labels)
+    epoches += 1
+    print(loss)
+
+top_5 = np.sort(row)[-5:]
+print(top_5)
+top5_indices = np.argpartition(row, -5)[-5:] # To get the top 5 values in descending order.
+top5_list = top5_indices.tolist()
+print(top5_indices)
+for i in token.id_to_word:
+    if i in top5_list:
+        print(token.id_to_word[i]) # Seeing what are the top 5 predicitions.
 
--- transformer/loss.py
+++ transformer/loss.py
@@ -8,6 +8,9 @@ class CrossEntropyLoss:
         self.total_mask_count = 0
         e_logits = np.exp(logits - np.max(logits, axis=1, keepdims=True))
         self.softmax = e_logits / np.sum(e_logits, axis=1, keepdims=True)
+
+## For Debugging purpose ##
+
         for position, label in enumerate(labels):
             if label == -100:
                 continue
@@ -15,10 +18,11 @@ class CrossEntropyLoss:
                 correct_class_prob = self.softmax[position, label]
                 total_loss += -np.log(correct_class_prob)
                 self.total_mask_count += 1
+                row  = self.logits[position] ## I have the row to check for the largest logits value, I am leaving it as it is for future debugging. 
         if self.total_mask_count > 0:
             loss = total_loss / self.total_mask_count
-        return loss
-
+        return loss, row
+###########################
     def backward(self):
         d_logits = self.softmax.copy()
         for position, label in enumerate(self.labels):
