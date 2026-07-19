f9fceffa8a6dfeb6f1649f63e8b3798b35619c16
deepanshu-013 <balharadeepanshu013@gmail.com>
2026-07-19 18:32:32 +0530

    Shut down the plain SGD.

--- transformer/sgd.py
+++ transformer/sgd.py
@@ -1,12 +1,12 @@
-class SGD:
-    def __init__(self, model, learning_rate= 0.01):
-        self.model = model
-        self.learning_rate = learning_rate
-
-    def step(self):
-        self.model.update(self.learning_rate)
-
-    def zero_grad(self):
-        self.model.zero_grad()
-
+# class SGD:
+#     def __init__(self, model, learning_rate= 0.01):
+#         self.model = model
+#         self.learning_rate = learning_rate
+#
+#     def step(self):
+#         self.model.update(self.learning_rate)
+#
+#     def zero_grad(self):
+#         self.model.zero_grad()
+#
 
